5e7cd43e02fa5fd84d12f70283c0b636391905c6
Vena <yppahhappy.com@gmail.com>
2024-02-27 11:53:09 +0100

    Learning CMake

---
+++ build/CMakeFiles/HonorsProject.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HonorsProject.dir/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/HonorsProject.dir/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/HonorsProject.dir/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/HonorsProject.dir/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/HonorsProject.dir/imgui/imgui.cpp.o"
+  "CMakeFiles/HonorsProject.dir/imgui/imgui.cpp.o.d"
+  "CMakeFiles/HonorsProject.dir/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/HonorsProject.dir/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/HonorsProject.dir/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/HonorsProject.dir/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/HonorsProject.dir/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/HonorsProject.dir/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/HonorsProject.dir/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/HonorsProject.dir/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/HonorsProject.dir/src/main.cpp.o"
+  "CMakeFiles/HonorsProject.dir/src/main.cpp.o.d"
+  "HonorsProject"
+  "HonorsProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HonorsProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ notes.md
@@ -0,0 +1,5 @@
+# imgui #
+
+`/honors/imgui/examples/example_glfw_opengl3$ ./example_glfw_opengl3`
+
+This is the example!
